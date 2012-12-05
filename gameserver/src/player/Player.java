d30862ebc1e6d624404c5900d005839a4ea57f2f
colinchen1984 <beyond0924>
2012-12-06 00:16:14 +0800

    .........

---
+++ gameserver/src/player/Player.java
@@ -0,0 +1,30 @@
+package player;
+
+import org.jboss.netty.channel.Channel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenlin
+ * Date: 12-12-5
+ * Time: 下午11:46
+ */
+final public class Player{
+
+	private Channel channel;
+
+
+
+	public static Player getPlayer(Channel channel){
+		Player result = new Player();
+		result.setChannel(channel);
+		return result;
+	}
+
+	public void setChannel(Channel channel){
+		this.channel = channel;
+	}
+
+	public Channel getChannel(){
+		return channel;
+	}
+}
